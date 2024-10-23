946a754b8708f0c77428cac295fc7ac37bb4bf1b
Thiago Piaia <edirlpiaia@gmail.com>
2024-10-23 16:38:07 -0300

    adjust component

--- src/components/ui/internal/content/index.tsx
+++ src/components/ui/internal/content/index.tsx
@@ -2,7 +2,7 @@ import { contentProps } from "./types/content-types";
 
 export function Content({ children }: contentProps) {
   return (
-    <div className="h-full flex-1 overflow-y-auto">
+    <div className="h-full flex-1 overflow-y-auto pb-2">
         {children}
     </div>
   )
